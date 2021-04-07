41f748bd4cfae5d478ae3c40de33405046822d6f
860430 <bert.deterd2@stedin.net>
2021-04-07 18:16:29 +0200

    init

--- template/src/index.js
+++ template/src/index.js
@@ -2,7 +2,7 @@ import '@ui5/webcomponents/dist/Assets.js';
 import '@ui5/webcomponents-fiori/dist/Assets.js';
 import '@ui5/webcomponents-react/dist/Assets';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
